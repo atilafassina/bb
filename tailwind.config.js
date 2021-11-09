028dd0567d9a7a3cda47b0c013334545aa737ae0
Atila Fassina <atila@fassina.eu>
2021-11-09 20:53:40 +0100

    fix: tailwind config - no spaces in glob

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./pages/**/*.{js, jsx, tsx, ts}",
-    "./components/**/*.{js, jsx, tsx, ts}",
-  ],
+  purge: ["./pages/**/*.{js,jsx,tsx,ts}", "./components/**/*.{js,jsx,tsx,ts}"],
 };
